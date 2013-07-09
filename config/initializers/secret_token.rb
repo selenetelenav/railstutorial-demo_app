474f3bc822ba8ae97277719519de0a369c9bccb1
Tan <selenet@mbp15-selenet.telenav.com>
2013-07-09 11:10:17 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '20c846425bc94cfef140f64c9da7d2ca061593cd0a438f188326241c37b52694c7ffa6507986b8ae45daa0ca736be09a4c9f11e2d2ff89ab9f8dd5c05ddde759'
